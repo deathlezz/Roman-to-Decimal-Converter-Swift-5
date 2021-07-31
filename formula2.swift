ce5b050ab03cf569b626fe0d5dd01b0b7bd75aa0
deathlezz <78265796+deathlezz@users.noreply.github.com>
2021-07-31 12:33:04 +0200

    Update formula2.swift

--- formula2.swift
+++ formula2.swift
@@ -4,13 +4,10 @@
 //  Created by deathlezz on 13/06/2021.
 //
 
-
 let dictionary: [( roman: String, decimal: Int )] = [
     ("M", 1000), ("CM", 900), ("D", 500), ("CD", 400),
     ("C", 100), ("XC", 90), ("L", 50), ("XL", 40),
-    ("X", 10), ("IX", 9), ("V", 5), ("IV", 4),
-    ("I", 1)]
-
+    ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1)]
 
 func decimalToRoman(value: Int) -> String {     // decimal -> roman
     
@@ -30,7 +27,6 @@ func decimalToRoman(value: Int) -> String {     // decimal -> roman
     }
     return result
 }
-
 
 func romanToDecimal(value: String) -> Int {     // roman -> decimal
     
