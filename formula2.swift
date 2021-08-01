e3011c43fac80050aba6a32b355a7e3387b28378
deathlezz <78265796+deathlezz@users.noreply.github.com>
2021-08-01 13:51:05 +0200

    Update formula2.swift

--- formula2.swift
+++ formula2.swift
@@ -4,19 +4,21 @@
 //  Created by deathlezz on 13/06/2021.
 //
 
-let dictionary: [( roman: String, decimal: Int )] = [
+// roman and decimal numbers array
+let array: [( roman: String, decimal: Int )] = [
     ("M", 1000), ("CM", 900), ("D", 500), ("CD", 400),
     ("C", 100), ("XC", 90), ("L", 50), ("XL", 40),
     ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1)]
 
-func decimalToRoman(value: Int) -> String {     // decimal -> roman
+// decimal -> roman
+func decimalToRoman(value: Int) -> String {
     
     var result = ""
     var number = value
     
     while number > 0 {
         
-        for (roman, decimal) in dictionary {
+        for (roman, decimal) in array {
             
             if number - decimal >= 0 {
                 result += roman
@@ -28,14 +30,15 @@ func decimalToRoman(value: Int) -> String {     // decimal -> roman
     return result
 }
 
-func romanToDecimal(value: String) -> Int {     // roman -> decimal
+// roman -> decimal
+func romanToDecimal(value: String) -> Int {
     
     var result = 0
     var number = value
 
     while number.count > 0 {
 
-        for (roman, decimal) in dictionary {
+        for (roman, decimal) in array {
             
             if number.starts(with: roman) {
                 result += decimal
